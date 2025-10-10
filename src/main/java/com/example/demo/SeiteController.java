57f751fff19eaf6e73f513089ea6033a53658d9c
pham-w <ha.pham@student.htw-berlin.de>
2025-10-10 16:31:07 +0200

    änderungen für Milestone 1

--- src/main/java/com/example/demo/SeiteController.java
+++ src/main/java/com/example/demo/SeiteController.java
@@ -1,14 +1,12 @@
 package com.example.demo;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
 import java.util.List;
 
 @Controller
 public class SeiteController {
     @GetMapping("/seite")
         public List<Seite> getProfile() {
-            return List.of(new Seite("M1",90, ));
+            return List.of(new Seite("Willhelm",90, 1.10.2000, "Blau", "backen", "pizza", "astronaut"));
     }
 }
