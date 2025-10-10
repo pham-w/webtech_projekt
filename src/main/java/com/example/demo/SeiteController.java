187ee3501b5a711aa92ad6be156f8cd278f66ae5
fakegingr <Angelina.Prigozhin@Student.HTW-Berlin.de>
2025-10-10 16:35:35 +0200

    .

--- src/main/java/com/example/demo/SeiteController.java
+++ src/main/java/com/example/demo/SeiteController.java
@@ -1,12 +1,14 @@
 package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
 public class SeiteController {
     @GetMapping("/seite")
         public List<Seite> getProfile() {
-            return List.of(new Seite("Willhelm",90, 1.10.2000, "Blau", "backen", "pizza", "astronaut"));
+            return List.of(new Seite("Willhelm",90,  LocalDate.of(2000, 10, 1), "Blau", "backen", "pizza", "astronaut"));
     }
 }
