0d17e03c9e7976147fccdad0ae0a2a0a028207f8
fakegingr <Angelina.Prigozhin@Student.HTW-Berlin.de>
2025-11-20 16:12:47 +0100

    Updates Controller

--- src/main/java/com/example/demo/SeiteController.java
+++ src/main/java/com/example/demo/SeiteController.java
@@ -1,4 +1,5 @@
 package com.example.demo;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -6,11 +7,15 @@ import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDate;
 import java.util.List;
 
-@RestController
 @CrossOrigin(origins = "https://frontend-freundebuch.onrender.com")
+@RestController
 public class SeiteController {
+
     @GetMapping("/seite")
-        public List<Seite> getProfile() {
-            return List.of(new Seite("Willhelm",90,  LocalDate.of(2000, 10, 1), "Blau", "backen", "pizza", "astronaut"));
+    public List<Seite> getProfile() {
+        return List.of(
+                new Seite("Willhelm", 90, LocalDate.of(2000, 10, 1),
+                        "Blau", "backen", "pizza", "astronaut")
+        );
     }
 }
